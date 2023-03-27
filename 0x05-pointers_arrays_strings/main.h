cadb27d6e238202114ed1ff6c3061e5672fe22b2
Tammyyy88 <josephtamilore13@gmail.com>
2023-03-27 07:58:51 -0700

    updated 3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 /**
  * reset_to_98 - Takes a pointer to an int and
  * updates the value it points to 98
@@ -8,6 +9,7 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @n: A pointer to an int
  */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
