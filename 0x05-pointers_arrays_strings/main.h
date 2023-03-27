a0ca9da387ce4b58a7c8971704012a75eb3e59f0
Tammyyy88 <josephtamilore13@gmail.com>
2023-03-27 04:53:23 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,8 +3,11 @@
 /**
  * reset_to_98 - Takes a pointer to an int and
  * updates the value it points to 98
+ * swap_int - swaps values of 2 integers
+ * _strlen - returna length of string
  * @n: A pointer to an int
  */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif /* MAIN_H */
