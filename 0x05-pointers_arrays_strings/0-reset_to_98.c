aea6f85d55359b0c27511d4826d9636f51d9a5a4
Tammyyy88 <josephtamilore13@gmail.com>
2023-03-27 04:32:14 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an int
+ * and updates value it points to 98
+ * @n: A pointer to an int
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * reset_to_98 - Takes a pointer to an int and
+ * updates the value it points to 98
+ * @n: A pointer to an int
+ */
+void reset_to_98(int *n);
+#endif /* MAIN_H */
