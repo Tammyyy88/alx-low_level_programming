b1190efbb31a2f6c74a2e85a4da76583dc924832
Tammyyy88 <josephtamilore13@gmail.com>
2023-04-06 17:46:40 -0700

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,41 @@
 #include "main.h"
+int _strlen_recursion(char *s);
+int pal_checker(char *s, int i, int j);
 /**
- * is_palindrome - Checks if a string is a palindrome
- * @s: The input string
- * Return: 1 if s is a palindrome, 0 otherwise
+ * _strlen_recursion - Returns the length of a string.
+ * @s: Pointer to the input string.
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * pal_checker - Checks if a string is a palindrome.
+ * @s: Pointer to the input string.
+ * @i: Left index.
+ * @j: Right index.
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int pal_checker(char *s, int i, int j)
+{
+if (i >= j)
+return (1);
+else if (s[i] != s[j])
+return (0);
+else
+return (pal_checker(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: Pointer to the input string.
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
-int len = 0;
-int start = 0;
-int end = 0;
-/* Find length of the string */
-while (s[len] != '\0')
-len++;
-end = len - 1;
-while (start < end)
-{
-if (s[start] != s[end])
-return (0);
-start++;
-end--;
-}
-return (1);
+int len = _strlen_recursion(s) - 1;
+return (pal_checker(s, 0, len));
 }
